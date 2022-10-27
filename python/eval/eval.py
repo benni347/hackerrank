dcb1439ff4fd4162aceee6a19fc8f028355804cf
Cedric Skwar <cdrc@skwar.me>
2022-10-27 15:42:21 +0200

    Added new problemes.

---
+++ python/eval/eval.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""
+:URL:	https://www.hackerrank.com/challenges/python-eval/problem
+"""
+
+if __name__ == "__main__":
+    eval(input())
--- python/invalid-regex/test_invalid_regex.py
+++ python/invalid-regex/test_invalid_regex.py
@@ -4,8 +4,29 @@ from invalid_regex import check
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_valid(self):
+        test_data = ((r".*\+", "True"), (r".*?", "True"), (r".*?", "True"))
+
+        for variant, data in enumerate(test_data, start=1):
+            regex_str, expected = data
+            with self.subTest(f'variation #{variant}',  neutrons_emitted=regex_str, expected=expected):
+                # pylint: disable=assignment-from-no-return
+                actual_result = check(regex_str)
+                failure_message = (f'Expected {expected} but returned {actual_result} '
+                                   f'regex_str={regex_str}')
+                self.assertEqual(actual_result, expected, failure_message)
+
+    def test_invalid(self):
+        test_data = ((r"*", "False"), (r"+*", "False"), (r".+*", "False"))
+
+        for variant, data in enumerate(test_data, start=1):
+            regex_str, expected = data
+            with self.subTest(f'variation #{variant}',  neutrons_emitted=regex_str, expected=expected):
+                # pylint: disable=assignment-from-no-return
+                actual_result = check(regex_str)
+                failure_message = (f'Expected {expected} but returned {actual_result} '
+                                   f'regex_str={regex_str}')
+                self.assertEqual(actual_result, expected, failure_message)
 
 
 if __name__ == '__main__':
