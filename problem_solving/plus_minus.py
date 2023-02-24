b913943b18bc961f242ff78a965b1cfdf4a177ad
Cédric Skwar <cdrc@skwar.me>
2023-02-24 23:33:14 +0100

    Added the plus minus problem

---
+++ problem_solving/plus_minus.py
@@ -0,0 +1,16 @@
+def plusMinus(arr):
+    n = len(arr)
+    positive = sum(1 for x in arr if x > 0)
+    negative = sum(1 for x in arr if x < 0)
+    zero = n - positive - negative
+    print(f"{positive/n:.6f}")
+    print(f"{negative/n:.6f}")
+    print(f"{zero/n:.6f}")
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
