7383acfa29746d07744e1e4f312d9e1fe5eb8618
Cedric Skwar <cdrc@5y5.one>
2023-09-24 13:17:50 +0200

    Changed the function calls

--- python/normal/time_conversion.py
+++ python/normal/time_conversion.py
@@ -5,24 +5,27 @@ This program converts a time from 12-hour AM/PM format to military (24-hour) tim
 Input: 07:05:45PM
 Output: 19:05:45
 """
+import os
+
 
 def timeConversion(s):
-    if s[-2:] == "AM" and s[:2] == "12":
+    if s.endswith("AM") and s.startswith("12"):
         return "00" + s[2:-2]
-    elif s[-2:] == "AM":
+    elif s.endswith("AM"):
         return s[:-2]
-    elif s[-2:] == "PM" and s[:2] == "12":
+    elif s.endswith("PM") and s.startswith("12"):
         return s[:-2]
     else:
         return str(int(s[:2]) + 12) + s[2:8]
 
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
 
     s = input()
 
     result = timeConversion(s)
 
-    fptr.write(result + '\n')
+    fptr.write(result + "\n")
 
     fptr.close()
